28011edb3ae6cb3f418088780d918cad657f0eec
lee-seung-hu <dltmdgn17@naver.com>
2025-07-15 15:11:44 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,18 +1,20 @@
 import selenium.webdriver as wb
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 import time
-import csv
-import os
+
+from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path="/usr/bin/chromedriver")
 
 options = Options()
-options.add_argument("--headless") # 윈도우 창을 안뛰우고 가능 
-options.add_argument("--no--sandbox")
+options.add_argument("--headless") # 창 없음
+options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 url = "https://pokemonkorea.co.kr/pokedex"
-driver = wb.Chrome(options=options)
+driver = wb.Chrome(service=service, options=options)
 driver.maximize_window()
 driver.get(url)
 img = driver.find_element(By.CSS_SELECTOR, "img.img-fluid")
@@ -20,16 +22,19 @@ img.click()
 time.sleep(1)
 h3 = driver.find_element(By.TAG_NAME, "h3")
 name = h3.text.split("\n")[1]
+
 driver.close()
 
+import csv
+import os
 pokemon_exist = os.path.exists("pokemon.csv")
-header = ["no","name"]
+header = ["no", "name"]
 
-with open("pokemon.csv", "a") as file:
-    writer =csv.writer(file)
+with open("pokemon.csv", "a", newline="") as file:
+    writer = csv.writer(file)
 
     if not pokemon_exist:
         writer.writerow(header)
 
-    writer.writerow(["0001",name])
-    print("csv생성완료")
+    writer.writerow(["0001", name])
+    print("포켓몬 저장 완료!!")
